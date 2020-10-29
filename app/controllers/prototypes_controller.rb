bb218f370d7a39555fc4caaa36e47771db10d09b
saitojun-creater <72370159+saitojun-creater@users.noreply.github.com>
2020-10-29 19:19:32 +0900

    未ログイン時のページ遷移の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show, :new]
 
   def index
     @prototypes = Prototype.all
